9daf9ff5bf4840ea06bd743b334ca779ab0d97a2
satinder-2000 <satinder_2000@outlook.com>
2022-11-06 07:54:37 +0000

    First Commit

---
+++ src/main/java/com/example/profileimage/ProfileImageDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.profileimage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfileImageDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfileImageDemoApplication.class, args);
+	}
+
+}
