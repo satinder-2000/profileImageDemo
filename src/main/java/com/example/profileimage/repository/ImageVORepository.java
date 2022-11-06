7ab7e8f48578dd182b154cdba5ccf596191a4c8a
satinder-2000 <satinder_2000@outlook.com>
2022-11-06 08:20:11 +0000

    First updates

---
+++ src/main/java/com/example/profileimage/repository/ImageVORepository.java
@@ -0,0 +1,8 @@
+package com.example.profileimage.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import com.example.profileimage.model.ImageVO;
+
+public interface ImageVORepository extends MongoRepository<ImageVO, String> {
+
+}
